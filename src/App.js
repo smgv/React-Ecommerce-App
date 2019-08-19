575e4b4cd14b51811e7ba7bf9bacf2c512cc633c
Patra <g.patra@accenture.com>
2019-08-19 21:30:23 +0530

    sign in, shop page and home page updated

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-// Testing Routes
-const HatsPage = props => {
-  console.log(props);
-  return <div>Hello World</div>;
-};
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-up/sign-in-and-up.component"
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./custom-button.styles.scss";
+
+const CustomButton = ({ children, ...buttonProps }) => (
+  <button className="custom-button" {...buttonProps}>
+    {children}
+  </button>
+);
+
+export default CustomButton;
