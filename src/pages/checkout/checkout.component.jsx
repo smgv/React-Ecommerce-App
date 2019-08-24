fdf2a0f8727e2d6e9c392523b935066e3c5ea640
Patra <g.patra@accenture.com>
2019-08-24 19:39:34 +0530

    test mode stripe is integrated with app

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from "reselect";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { selectCartTotal } from "../../redux/cart/cart.selectors";
 import CheckoutCartItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -29,6 +30,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutCartItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL : ${total}</div>
+    <div className="test-warning">
+    *Please use the following test credit card for payments*
+    <br />
+    4242 4242 4242 4242 - EXP: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
